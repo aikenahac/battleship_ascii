2ff9a94ed279c3cf0054e5d3b0d8d0a36601f6b0
Aiken Tine Ahac <ahac.aiken@gmail.com>
2020-02-25 20:26:48 +0100

    yes

--- battleship.cpp
+++ battleship.cpp
@@ -3,10 +3,14 @@
 #include <cstdlib>
 #include <cstdio>
 #include <ctime>
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__TOS_WIN__) || defined(__WINDOWS__)
+    #include <windows.h>
+#endif
 using namespace std;
 
 void polja(char player_field[][12], char computer_field[][12], int n){
-	system("clear");
+	system("clear"); //na sistemu Windows dodaj komentar
+	//system("cls"); //na sistemu Windows odstrani komentar 
 	for (int i=0;i<n;i++){
 		for(int j=0;j<12;j++){
 			cout << setw(2) << player_field[i][j];
@@ -25,9 +29,9 @@ void polja(char player_field[][12], char computer_field[][12], int n){
 void postavitev_player(char player_field[][12], int len){
 	int coordX, coordY;
 	cout << "Vpisi x koordinato: ";
-	cin >> coordX;
-	cout << "Vpisi y koordinato: ";
 	cin >> coordY;
+	cout << "Vpisi y koordinato: ";
+	cin >> coordX;
 	cout << endl;
 	if(coordX >= 9){
 		coordX = coordX - len + 1;
@@ -53,34 +57,6 @@ void clearTable(char field[][12]){
 	}
 }
 
-void igralec_ugiba(char computer_field[][12], char player_field[][12]){
-	int x, y;
-	cout << "Vpisi x: ";
-	cin >> x;
-	cout << "Vpisi y: ";
-	cin >> y;
-	if(computer_field[x][y] == 'X'){
-		computer_field[x][y] = '@';
-		igralec_ugiba(computer_field, player_field);
-	}else{
-		computer_field[x][y] = '#';
-		racunalnik_ugiba(player_field, computer_field);
-	}
-
-}
-
-void racunalnik_ugiba(char player_field[][12], char computer_field[][12]){
-	int x = rand()%12;
-	int y = rand()%12;
-	if(player_field[x][y] == 'X'){
-		player_field[x][y] = '@';
-		racunalnik_ugiba(player_field, computer_field);
-	}else{
-		computer_field[x][y] = '#';
-		igralec_ugiba(computer_field, player_field);
-	}
-}
-
 int main(){
 	srand(time(NULL));
 	char player_field[12][12];
@@ -97,7 +73,59 @@ int main(){
 	postavitev_computer(computer_field, 2);
 	postavitev_computer(computer_field, 2);
 	polja(player_field, computer_field, 12);
-	igralec_ugiba(computer_field, player_field);
-	polja(player_field, computer_field, 12);
+
+	bool winPC, winPlayer, ugibanjePlayer = true, ugibanjePC = false;
+	int playerX, playerY,pcX, pcY, stevecPC = 0, stevecPlayer = 0;
+
+	while(true){
+		while(ugibanjePlayer){
+			cout << "Vpisi x in y na katerega bi ciljal (oba med 0 in 11): \n";
+			cout << "x: ";
+			cin >> playerX;
+			cout << "y: ";
+			cin >> playerY;
+			cout << endl;
+			if(computer_field[playerX][playerY] == 'X'){
+				computer_field[playerX][playerY] = '@';
+				stevecPlayer++;
+				cout << "Zadel si!\n";
+				if(stevecPlayer==7){
+					winPlayer = true;
+					winPC = false;
+					break;
+				}
+			}else {
+				computer_field[playerX][playerY] = '#';
+				ugibanjePlayer = false;
+				ugibanjePC = true;
+				break;
+			}
+		}
+		while(ugibanjePC){
+			pcX = rand()%12;
+			pcY = rand()%12;
+			if(player_field[pcX][pcY] == 'X'){
+				player_field[pcX][pcY] = '@';
+				stevecPC++;
+				if(stevecPC == 7){
+					winPC = true;
+					winPlayer = false;
+					break;
+				}
+			}else {
+				player_field[pcX][pcY] = '#';
+				ugibanjePC = false;
+				ugibanjePlayer = true;
+				break;
+			}
+		}
+		if(winPlayer){
+			cout << "Bravo! Zmagal si!\n";
+			break;
+		}else if(winPC){
+			cout << "Izgubil si... Vec srece prihodnjic!\n";
+		}
+	}
+	polja(player_field, computer_field, 12);	
 	return 0;
 } 	
